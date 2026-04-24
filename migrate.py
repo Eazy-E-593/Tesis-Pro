c2755eb540f06b1c55dbae3bff694770ff2bda7d
Johao Castillo <wjcm.0604952291@gmail.com>
2026-04-24 16:37:59 -0500

    cleanup: eliminacion definitiva de archivos legacy de sqlite

--- migrate.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('microbase.db')
-c = conn.cursor()
-c.execute("UPDATE app_fields SET name = 'Unidad de Venta' WHERE name = 'Unidad'")
-conn.commit()
-conn.close()
-print("Migración completada")
